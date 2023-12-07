86cf032ffa30817696cde86bf3d0cc5f1b183d86
tangbanbonghoa <151010788+tangbanbonghoa@users.noreply.github.com>
2023-12-08 01:12:54 +0700

    Add files via upload

---
+++ BanGiay/Form/Frm_Dialog/DangNhap_Frm.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace PRL.Frm_Main
+{
+    partial class DangNhap_Frm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DangNhap_Frm));
+            txtQuenMatKhau = new Label();
+            txtMatKhau = new TextBox();
+            txt2 = new Label();
+            txt1 = new Label();
+            pictureBox1 = new PictureBox();
+            btnDangnhap = new Button();
+            txt4 = new Label();
+            txt3 = new Label();
+            txtTaiKhoan = new TextBox();
+            ptbTat = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)ptbTat).BeginInit();
+            SuspendLayout();
+            // 
+            // txtQuenMatKhau
+            // 
+            txtQuenMatKhau.AutoSize = true;
+            txtQuenMatKhau.Cursor = Cursors.Hand;
+            txtQuenMatKhau.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            txtQuenMatKhau.Location = new Point(457, 321);
+            txtQuenMatKhau.Name = "txtQuenMatKhau";
+            txtQuenMatKhau.Size = new Size(108, 17);
+            txtQuenMatKhau.TabIndex = 32;
+            txtQuenMatKhau.Text = "Quên mật khẩu?";
+            txtQuenMatKhau.Click += btnQuenmk_Click;
+            // 
+            // txtMatKhau
+            // 
+            txtMatKhau.BorderStyle = BorderStyle.FixedSingle;
+            txtMatKhau.Font = new Font("Calibri", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            txtMatKhau.Location = new Point(318, 207);
+            txtMatKhau.Name = "txtMatKhau";
+            txtMatKhau.PasswordChar = '*';
+            txtMatKhau.Size = new Size(277, 23);
+            txtMatKhau.TabIndex = 25;
+            txtMatKhau.UseSystemPasswordChar = true;
+            // 
+            // txt2
+            // 
+            txt2.AutoSize = true;
+            txt2.Font = new Font("Calibri", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            txt2.Location = new Point(318, 70);
+            txt2.Margin = new Padding(2, 0, 2, 0);
+            txt2.Name = "txt2";
+            txt2.Size = new Size(251, 18);
+            txt2.TabIndex = 31;
+            txt2.Text = "Vui lòng nhập thông tin chi tiết của bạn!";
+            // 
+            // txt1
+            // 
+            txt1.AutoSize = true;
+            txt1.FlatStyle = FlatStyle.Popup;
+            txt1.Font = new Font("Calibri", 30F, FontStyle.Bold, GraphicsUnit.Point);
+            txt1.Location = new Point(310, 21);
+            txt1.Margin = new Padding(2, 0, 2, 0);
+            txt1.Name = "txt1";
+            txt1.Size = new Size(285, 49);
+            txt1.TabIndex = 30;
+            txt1.Text = "Chào mừng bạn";
+            txt1.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(5, -1);
+            pictureBox1.Margin = new Padding(2);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(290, 400);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 29;
+            pictureBox1.TabStop = false;
+            // 
+            // btnDangnhap
+            // 
+            btnDangnhap.FlatAppearance.BorderSize = 0;
+            btnDangnhap.FlatStyle = FlatStyle.Flat;
+            btnDangnhap.Font = new Font("Calibri", 18.75F, FontStyle.Bold, GraphicsUnit.Point);
+            btnDangnhap.Location = new Point(307, 257);
+            btnDangnhap.Name = "btnDangnhap";
+            btnDangnhap.Size = new Size(398, 49);
+            btnDangnhap.TabIndex = 26;
+            btnDangnhap.Text = "Đăng nhập";
+            btnDangnhap.UseVisualStyleBackColor = true;
+            btnDangnhap.Click += btnDangnhap_Click;
+            // 
+            // txt4
+            // 
+            txt4.AutoSize = true;
+            txt4.Font = new Font("Calibri", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            txt4.Location = new Point(318, 185);
+            txt4.Name = "txt4";
+            txt4.Size = new Size(79, 19);
+            txt4.TabIndex = 27;
+            txt4.Text = "Mật khẩu:";
+            // 
+            // txt3
+            // 
+            txt3.AutoSize = true;
+            txt3.Font = new Font("Calibri", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            txt3.Location = new Point(318, 110);
+            txt3.Name = "txt3";
+            txt3.Size = new Size(106, 19);
+            txt3.TabIndex = 28;
+            txt3.Text = "Tên tài khoản:";
+            // 
+            // txtTaiKhoan
+            // 
+            txtTaiKhoan.BorderStyle = BorderStyle.FixedSingle;
+            txtTaiKhoan.Font = new Font("Calibri", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            txtTaiKhoan.Location = new Point(318, 132);
+            txtTaiKhoan.Margin = new Padding(10);
+            txtTaiKhoan.Name = "txtTaiKhoan";
+            txtTaiKhoan.Size = new Size(277, 26);
+            txtTaiKhoan.TabIndex = 24;
+            // 
+            // ptbTat
+            // 
+            ptbTat.Cursor = Cursors.Hand;
+            ptbTat.Image = BanGiay.Properties.Resources.power_switch;
+            ptbTat.Location = new Point(671, 12);
+            ptbTat.Name = "ptbTat";
+            ptbTat.Size = new Size(34, 28);
+            ptbTat.SizeMode = PictureBoxSizeMode.Zoom;
+            ptbTat.TabIndex = 33;
+            ptbTat.TabStop = false;
+            ptbTat.Click += ptbTat_Click;
+            // 
+            // DangNhap_Frm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(711, 398);
+            Controls.Add(ptbTat);
+            Controls.Add(txtQuenMatKhau);
+            Controls.Add(txtMatKhau);
+            Controls.Add(txt2);
+            Controls.Add(txt1);
+            Controls.Add(pictureBox1);
+            Controls.Add(btnDangnhap);
+            Controls.Add(txt4);
+            Controls.Add(txt3);
+            Controls.Add(txtTaiKhoan);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "DangNhap_Frm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "DangNhap_Form";
+            Load += DangNhap_Frm_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)ptbTat).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label txtQuenMatKhau;
+        private TextBox txtMatKhau;
+        private Label txt2;
+        private Label txt1;
+        private PictureBox pictureBox1;
+        private Button btnDangnhap;
+        private Label txt4;
+        private Label txt3;
+        private TextBox txtTaiKhoan;
+        private PictureBox ptbTat;
+    }
+}
---
+++ BanGiay/Form/Frm_Dialog/QuenMatKhau_Frm.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace PRL.Frm_Main
+{
+    partial class QuenMatKhau_Frm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupXacNhan = new GroupBox();
+            btnXacNhan_MaXacMinh = new Button();
+            btnGuiMa = new Button();
+            txtMaXacMinh = new TextBox();
+            label3 = new Label();
+            txtEmail = new TextBox();
+            label2 = new Label();
+            label1 = new Label();
+            groupBox2 = new GroupBox();
+            btnXacNhan_DoiMatKhau = new Button();
+            txtMatKhauMoi_2 = new TextBox();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            txtMatKhauMoi_1 = new TextBox();
+            groupXacNhan.SuspendLayout();
+            groupBox2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupXacNhan
+            // 
+            groupXacNhan.Controls.Add(btnXacNhan_MaXacMinh);
+            groupXacNhan.Controls.Add(btnGuiMa);
+            groupXacNhan.Controls.Add(txtMaXacMinh);
+            groupXacNhan.Controls.Add(label3);
+            groupXacNhan.Controls.Add(txtEmail);
+            groupXacNhan.Controls.Add(label2);
+            groupXacNhan.Controls.Add(label1);
+            groupXacNhan.Location = new Point(12, 12);
+            groupXacNhan.Name = "groupXacNhan";
+            groupXacNhan.Size = new Size(363, 368);
+            groupXacNhan.TabIndex = 0;
+            groupXacNhan.TabStop = false;
+            // 
+            // btnXacNhan_MaXacMinh
+            // 
+            btnXacNhan_MaXacMinh.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            btnXacNhan_MaXacMinh.Location = new Point(219, 242);
+            btnXacNhan_MaXacMinh.Name = "btnXacNhan_MaXacMinh";
+            btnXacNhan_MaXacMinh.Size = new Size(120, 40);
+            btnXacNhan_MaXacMinh.TabIndex = 3;
+            btnXacNhan_MaXacMinh.Text = "Xác nhận";
+            btnXacNhan_MaXacMinh.UseVisualStyleBackColor = true;
+            btnXacNhan_MaXacMinh.Click += btnXacNhan_MaXacMinh_Click;
+            // 
+            // btnGuiMa
+            // 
+            btnGuiMa.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            btnGuiMa.Location = new Point(219, 122);
+            btnGuiMa.Name = "btnGuiMa";
+            btnGuiMa.Size = new Size(120, 40);
+            btnGuiMa.TabIndex = 1;
+            btnGuiMa.Text = "Gửi mã";
+            btnGuiMa.UseVisualStyleBackColor = true;
+            btnGuiMa.Click += btnGuiMa_Click;
+            // 
+            // txtMaXacMinh
+            // 
+            txtMaXacMinh.Location = new Point(21, 202);
+            txtMaXacMinh.Multiline = true;
+            txtMaXacMinh.Name = "txtMaXacMinh";
+            txtMaXacMinh.Size = new Size(318, 34);
+            txtMaXacMinh.TabIndex = 2;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(21, 184);
+            label3.Name = "label3";
+            label3.Size = new Size(240, 15);
+            label3.TabIndex = 0;
+            label3.Text = "Nhập mã xác nhận đã được gửi trong email:";
+            // 
+            // txtEmail
+            // 
+            txtEmail.Location = new Point(21, 82);
+            txtEmail.Multiline = true;
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(318, 34);
+            txtEmail.TabIndex = 0;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(21, 64);
+            label2.Name = "label2";
+            label2.Size = new Size(116, 15);
+            label2.TabIndex = 0;
+            label2.Text = "Nhập Email của bạn:";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(90, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(175, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Xác nhận thông tin";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(btnXacNhan_DoiMatKhau);
+            groupBox2.Controls.Add(txtMatKhauMoi_2);
+            groupBox2.Controls.Add(label4);
+            groupBox2.Controls.Add(label5);
+            groupBox2.Controls.Add(label6);
+            groupBox2.Controls.Add(txtMatKhauMoi_1);
+            groupBox2.Location = new Point(381, 12);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(363, 368);
+            groupBox2.TabIndex = 0;
+            groupBox2.TabStop = false;
+            // 
+            // btnXacNhan_DoiMatKhau
+            // 
+            btnXacNhan_DoiMatKhau.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            btnXacNhan_DoiMatKhau.Location = new Point(218, 196);
+            btnXacNhan_DoiMatKhau.Name = "btnXacNhan_DoiMatKhau";
+            btnXacNhan_DoiMatKhau.Size = new Size(120, 40);
+            btnXacNhan_DoiMatKhau.TabIndex = 6;
+            btnXacNhan_DoiMatKhau.Text = "Xác nhận";
+            btnXacNhan_DoiMatKhau.UseVisualStyleBackColor = true;
+            btnXacNhan_DoiMatKhau.Click += btnXacNhan_DoiMatKhau_Click;
+            // 
+            // txtMatKhauMoi_2
+            // 
+            txtMatKhauMoi_2.Location = new Point(20, 156);
+            txtMatKhauMoi_2.Multiline = true;
+            txtMatKhauMoi_2.Name = "txtMatKhauMoi_2";
+            txtMatKhauMoi_2.PasswordChar = '*';
+            txtMatKhauMoi_2.Size = new Size(318, 34);
+            txtMatKhauMoi_2.TabIndex = 5;
+            txtMatKhauMoi_2.UseSystemPasswordChar = true;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI Semibold", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(132, 19);
+            label4.Name = "label4";
+            label4.Size = new Size(128, 25);
+            label4.TabIndex = 0;
+            label4.Text = "Đổi mật khẩu";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(20, 64);
+            label5.Name = "label5";
+            label5.Size = new Size(161, 15);
+            label5.TabIndex = 0;
+            label5.Text = "Nhập mật khẩu mới của bạn:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(20, 138);
+            label6.Name = "label6";
+            label6.Size = new Size(151, 15);
+            label6.TabIndex = 0;
+            label6.Text = "Xác nhận lại mật khẩu mới:";
+            // 
+            // txtMatKhauMoi_1
+            // 
+            txtMatKhauMoi_1.Location = new Point(20, 82);
+            txtMatKhauMoi_1.Multiline = true;
+            txtMatKhauMoi_1.Name = "txtMatKhauMoi_1";
+            txtMatKhauMoi_1.PasswordChar = '*';
+            txtMatKhauMoi_1.Size = new Size(318, 34);
+            txtMatKhauMoi_1.TabIndex = 4;
+            txtMatKhauMoi_1.UseSystemPasswordChar = true;
+            // 
+            // QuenMatKhau_Frm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(754, 392);
+            Controls.Add(groupBox2);
+            Controls.Add(groupXacNhan);
+            Name = "QuenMatKhau_Frm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Quên Mật Khẩu";
+            FormClosed += QuenMatKhau_Frm_FormClosed;
+            Load += QuenMatKhau_Frm_Load;
+            groupXacNhan.ResumeLayout(false);
+            groupXacNhan.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupXacNhan;
+        private TextBox txtMaXacMinh;
+        private Label label3;
+        private TextBox txtEmail;
+        private Label label2;
+        private Label label1;
+        private GroupBox groupBox2;
+        private Button btnXacNhan_MaXacMinh;
+        private Button btnGuiMa;
+        private Button btnXacNhan_DoiMatKhau;
+        private TextBox txtMatKhauMoi_2;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private TextBox txtMatKhauMoi_1;
+    }
+}
